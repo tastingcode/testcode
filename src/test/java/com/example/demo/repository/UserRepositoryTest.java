4bdc32fef96325330913c397881355624a3e8ef1
taco <wiseitrees@gmail.com>
2025-12-01 17:03:45 +0900

    UserRepository 테스트

---
+++ src/test/java/com/example/demo/repository/UserRepositoryTest.java
@@ -0,0 +1,69 @@
+package com.example.demo.repository;
+
+import com.example.demo.model.UserStatus;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.jdbc.Sql;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+// DataJpaTest에 내장되어 있음
+// @ExtendWith(SpringExtension.class)
+// 기본 설정을 따라도 돼서 필요가 없었음
+// @TestPropertySource("classpath:test-application.properties")
+@DataJpaTest(showSql = true)
+@Sql("/sql/user-repository-test-data.sql")
+public class UserRepositoryTest {
+
+	@Autowired
+	private UserRepository userRepository;
+
+	@Test
+	void findByIdAndStatus_로_유저_데이터를_찾아올_수_있다() {
+		// given
+
+		// when
+		Optional<UserEntity> result = userRepository.findByIdAndStatus(1, UserStatus.ACTIVE);
+
+		// then
+		assertThat(result.isPresent()).isTrue();
+	}
+
+	@Test
+	void findByIdAndStatus_는_데이터가_없으면_Optional_emtpy_를_내려준다() {
+		// given
+
+		// when
+		Optional<UserEntity> result = userRepository.findByIdAndStatus(1, UserStatus.PENDING);
+
+		// then
+		assertThat(result.isEmpty()).isTrue();
+	}
+
+	@Test
+	void findByEmailAndStatus_로_유저_데이터를_찾아올_수_있다() {
+		// given
+
+		// when
+		Optional<UserEntity> result = userRepository.findByEmailAndStatus("kok202@naver.com", UserStatus.ACTIVE);
+
+		// then
+		assertThat(result.isPresent()).isTrue();
+	}
+
+	@Test
+	void findByEmailAndStatus_는_데이터가_없으면_Optional_emtpy_를_내려준다() {
+		// given
+
+		// when
+		Optional<UserEntity> result = userRepository.findByEmailAndStatus("kok202@naver.com", UserStatus.PENDING);
+
+		// then
+		assertThat(result.isEmpty()).isTrue();
+	}
+
+
+}
