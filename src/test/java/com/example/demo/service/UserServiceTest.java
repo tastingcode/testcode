6aa7719b66c0f632887af22b8358d5a0cd516bb0
taco <wiseitrees@gmail.com>
2025-12-01 17:43:46 +0900

    UserService 메서드에 테스트 코드 추가

--- src/test/java/com/example/demo/service/UserServiceTest.java
+++ src/test/java/com/example/demo/service/UserServiceTest.java
@@ -9,7 +9,6 @@ import com.example.demo.repository.UserEntity;
 import org.junit.jupiter.api.Test;
 import org.mockito.BDDMockito;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.mail.SimpleMailMessage;
@@ -32,7 +31,8 @@ public class UserServiceTest {
 
 	@Autowired
 	UserService userService;
-
+	@MockBean
+	private JavaMailSender mailSender;
 
 	@Test
 	void getByEmail은_ACTIVE_상태인_유저를_찾아올_수_있다() {
@@ -78,6 +78,75 @@ public class UserServiceTest {
 		}).isInstanceOf(ResourceNotFoundException.class);
 	}
 
+	@Test
+	void userCreateDto_를_이용하여_유저를_생성할_수_있다() {
+		// given
+		UserCreateDto userCreateDto = UserCreateDto.builder()
+				.email("kok202@kakao.com")
+				.address("Gyeongi")
+				.nickname("kok202-k")
+				.build();
+		BDDMockito.doNothing().when(mailSender).send(any(SimpleMailMessage.class));
+
+		// when
+		UserEntity result = userService.create(userCreateDto);
+
+		// then
+		assertThat(result.getId()).isNotNull();
+		assertThat(result.getStatus()).isEqualTo(UserStatus.PENDING);
+		// assertThat(result.getCertificationCode()).isEqualTo("T.T"); // FIXME
+	}
+
+	@Test
+	void userUpdateDto_를_이용하여_유저를_수정할_수_있다() {
+		// given
+		UserUpdateDto userUpdateDto = UserUpdateDto.builder()
+				.address("Incheon")
+				.nickname("kok202-n")
+				.build();
+
+		// when
+		userService.update(1, userUpdateDto);
+
+		// then
+		UserEntity userEntity = userService.getById(1);
+		assertThat(userEntity.getId()).isNotNull();
+		assertThat(userEntity.getAddress()).isEqualTo("Incheon");
+		assertThat(userEntity.getNickname()).isEqualTo("kok202-n");
+	}
+
+	@Test
+	void user를_로그인_시키면_마지막_로그인_시간이_변경된다() {
+		// given
+		// when
+		userService.login(1);
+
+		// then
+		UserEntity userEntity = userService.getById(1);
+		assertThat(userEntity.getLastLoginAt()).isGreaterThan(0L);
+		// assertThat(result.getLastLoginAt()).isEqualTo("T.T"); // FIXME
+	}
+
+	@Test
+	void PENDING_상태의_사용자는_인증_코드로_ACTIVE_시킬_수_있다() {
+		// given
+		// when
+		userService.verifyEmail(2, "aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaab");
+
+		// then
+		UserEntity userEntity = userService.getById(2);
+		assertThat(userEntity.getStatus()).isEqualTo(UserStatus.ACTIVE);
+	}
+
+	@Test
+	void PENDING_상태의_사용자는_잘못된_인증_코드를_받으면_에러를_던진다() {
+		// given
+		// when
+		// then
+		assertThatThrownBy(() -> {
+			userService.verifyEmail(2, "aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaac");
+		}).isInstanceOf(CertificationCodeNotMatchedException.class);
+	}
 
 
 }
