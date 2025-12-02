8c4501d75ae757eb9a4a8f1a594117945c31780d
taco <wiseitrees@gmail.com>
2025-12-02 15:30:16 +0900

    Repository 의존성 역전 적용

--- src/test/java/com/example/demo/user/controller/UserControllerTest.java
+++ src/test/java/com/example/demo/user/controller/UserControllerTest.java
@@ -3,7 +3,7 @@
 import com.example.demo.user.domain.UserStatus;
 import com.example.demo.user.domain.UserUpdate;
 import com.example.demo.user.infrastructure.UserEntity;
-import com.example.demo.user.infrastructure.UserRepository;
+import com.example.demo.user.infrastructure.UserJpaRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -32,7 +32,7 @@ public class UserControllerTest {
 	@Autowired
 	private MockMvc mockMvc;
 	@Autowired
-	private UserRepository userRepository;
+	private UserJpaRepository userJpaRepository;
 	private final ObjectMapper objectMapper = new ObjectMapper();
 
 	@Test
@@ -68,7 +68,7 @@ public class UserControllerTest {
 						get("/api/users/2/verify")
 								.queryParam("certificationCode", "aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaab"))
 				.andExpect(status().isFound());
-		UserEntity userEntity = userRepository.findById(1L).get();
+		UserEntity userEntity = userJpaRepository.findById(1L).get();
 		assertThat(userEntity.getStatus()).isEqualTo(UserStatus.ACTIVE);
 	}
 
--- src/test/java/com/example/demo/user/infrastructure/UserJpaRepositoryTest.java
+++ src/test/java/com/example/demo/user/infrastructure/UserJpaRepositoryTest.java
@@ -16,17 +16,17 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 // @TestPropertySource("classpath:test-application.properties")
 @DataJpaTest(showSql = true)
 @Sql("/sql/user-repository-test-data.sql")
-public class UserRepositoryTest {
+public class UserJpaRepositoryTest {
 
 	@Autowired
-	private UserRepository userRepository;
+	private UserJpaRepository userJpaRepository;
 
 	@Test
 	void findByIdAndStatus_로_유저_데이터를_찾아올_수_있다() {
 		// given
 
 		// when
-		Optional<UserEntity> result = userRepository.findByIdAndStatus(1, UserStatus.ACTIVE);
+		Optional<UserEntity> result = userJpaRepository.findByIdAndStatus(1, UserStatus.ACTIVE);
 
 		// then
 		assertThat(result.isPresent()).isTrue();
@@ -37,7 +37,7 @@ public class UserRepositoryTest {
 		// given
 
 		// when
-		Optional<UserEntity> result = userRepository.findByIdAndStatus(1, UserStatus.PENDING);
+		Optional<UserEntity> result = userJpaRepository.findByIdAndStatus(1, UserStatus.PENDING);
 
 		// then
 		assertThat(result.isEmpty()).isTrue();
@@ -48,7 +48,7 @@ public class UserRepositoryTest {
 		// given
 
 		// when
-		Optional<UserEntity> result = userRepository.findByEmailAndStatus("kok202@naver.com", UserStatus.ACTIVE);
+		Optional<UserEntity> result = userJpaRepository.findByEmailAndStatus("kok202@naver.com", UserStatus.ACTIVE);
 
 		// then
 		assertThat(result.isPresent()).isTrue();
@@ -59,7 +59,7 @@ public class UserRepositoryTest {
 		// given
 
 		// when
-		Optional<UserEntity> result = userRepository.findByEmailAndStatus("kok202@naver.com", UserStatus.PENDING);
+		Optional<UserEntity> result = userJpaRepository.findByEmailAndStatus("kok202@naver.com", UserStatus.PENDING);
 
 		// then
 		assertThat(result.isEmpty()).isTrue();
