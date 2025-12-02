12b1fb48113b59e00b8ace30acca70067cad2eed
taco <wiseitrees@gmail.com>
2025-12-02 17:57:20 +0900

    테스트 코드 보강 및 Clock, Uuid 처리

--- src/test/java/com/example/demo/post/domain/PostTest.java
+++ src/test/java/com/example/demo/post/domain/PostTest.java
@@ -1,18 +1,39 @@
 package com.example.demo.post.domain;
 
+import com.example.demo.user.domain.User;
+import com.example.demo.user.domain.UserStatus;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 class PostTest {
 
 	@Test
 	public void PostCreate으로_게시물을_만들_수_있다() {
 		//given
+		PostCreate postCreate = PostCreate.builder()
+				.writerId(1)
+				.content("helloworld")
+				.build();
+		User writer = User.builder()
+				.id(1L)
+				.email("kok202@naver.com")
+				.nickname("kok202")
+				.address("Seoul")
+				.status(UserStatus.ACTIVE)
+				.certificationCode("aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaab")
+				.build();
 
 		//when
+		Post post = Post.from(writer, postCreate);
 
 		//then
+		assertThat(post.getContent()).isEqualTo("helloworld");
+		assertThat(post.getWriter().getEmail()).isEqualTo("kok202@naver.com");
+		assertThat(post.getWriter().getNickname()).isEqualTo("kok202");
+		assertThat(post.getWriter().getAddress()).isEqualTo("Seoul");
+		assertThat(post.getWriter().getStatus()).isEqualTo(UserStatus.ACTIVE);
+		assertThat(post.getWriter().getCertificationCode()).isEqualTo("aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaab");
 
 	}
 
